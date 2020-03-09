9ce0cd0029c0455a0904dbc346d35f9d3816f269
Alingo2 <762480390@qq.com>
2020-03-09 21:25:49 +0800

    position

--- root_bone.py
+++ root_bone.py
@@ -2,7 +2,7 @@ from cocos.skeleton import Bone, Skeleton
 
 def Point2(*args): return args
 
-root_bone = Bone('body', 70, -185.13746808221953, Point2(37.00, -7.00))
+root_bone = Bone('body', 70, -186.88284572663372, Point2(2.00, 23.00))
 
 
 skeleton = Skeleton( root_bone )
--- root_skin.py
+++ root_skin.py
@@ -1,4 +1,4 @@
 
 skin = [
-    ('body', (10, 96), 'gil-cuerpo.png', True, True, 0.5),
+    ('body', (-28, 112), 'gil-cuerpo.png', True, True, 0.5),
   ]
