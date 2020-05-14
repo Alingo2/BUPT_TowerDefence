dd7d7fbf2632269bdcb3a695aa5b8a80e2d92b62
supotato6 <1322749946@qq.com>
2020-05-14 11:18:45 +0800

    left_e

--- animation/model2_skin.py
+++ animation/model2_skin.py
@@ -7,7 +7,6 @@ skin = [
     ('brazo izq', (10, 49), 'gil-mano2.png', False, False, 0.5),
     ('muslo izq', (10, 70), 'gil-bota2.png', False, False, 0.5),
     #('pierna izq', (10, 75), 'gil-bota2.png', False, False, 0.5),
-    ('cabeza', (31, 44), 'gil-peluca.png', True, True, 0.5),
     ('torso', (25, 91), 'gil-cuerpo.png', True, True, 0.5),
     ('cabeza', (16, 18), 'gil-cara.png', True, True, 0.5),
     ('antebrazo der', (33, 5), 'pistal1.png', False, False, 0.4),
---
+++ animation/myE_skin.py
@@ -0,0 +1,21 @@
+from __future__ import division, print_function, unicode_literals
+#with open(r"D:/代码编辑器/SoulOfPython/Lib/site-packages/cocos/resources/data.txt", "r") as f:  # 打开文件
+skin = [
+    ('muslo izq', (10, 55), 'turn_t_Rleg.png', False, False, 0.5),
+    ('muslo der', (10, 55), 'turn_t_Lleg.png', False, False, 0.5),
+    ('torso', (25, 91), 'turn_t_body.png', True, True, 0.5),
+    #('pierna izq', (10, 75), 'gil-bota2.png', False, False, 0.5),
+    ('armor', (0, 0), 'turn_suit_2.png', False, False, 0.74),
+    ('cabeza', (31, 44), 'turn_t_head.png', True, True, 0.5),
+    ('helmet', (0, 0), 'turn_helmet_2.png', False, False, 0.65),
+    ('antebrazo der', (33, 5), 'turn_t_sword.png', False, False, 0.4),
+
+    #('pierna der', (15, 67), 'gil-bota1.png', False, False, 0.5),
+  ]
+
+
+
+# skin = [
+#     ('torso', (45, 70), 'body.png', True, True, 0.5),
+#     ('cabeza', (16, -5), 'gil-cara.png', True, True, 0.5),
+#   ]
